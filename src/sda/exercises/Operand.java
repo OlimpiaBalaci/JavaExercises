a72f0f3144d6a9070151f436577e41423568088e
CiobanuB <54896362+CiobanuB@users.noreply.github.com>
2020-01-12 13:13:17 +0200

    Update Operand.java

--- src/sda/exercises/Operand.java
+++ src/sda/exercises/Operand.java
@@ -1,4 +1,23 @@
 package sda.exercises;
 
 public class Operand {
+  private Object left;
+    private Object right;
+    private String operator;
+
+    public Operand(String expresion) {
+        expresion = expresion.substring(1,expresion.length()-1);
+        String[] operator = {">","<","<=",">=","==","!="};
+        for (int i = 0; i < operator.length ; i++) {
+            String[] result = expresion.split(operator[i]);
+            if(result.length != 0)
+            {
+                this.left = result[0];
+                this.right = result[1];
+                this.operator = operator[i];
+                break;
+            }
+        }
+
+    }
 }
