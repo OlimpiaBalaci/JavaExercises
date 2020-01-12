c50baddef6ad8d1dc663660bd43a9506cbbc665e
Olimpia Balaci <olimpia.balaci@yahoo.com>
2020-01-12 12:08:42 +0200

    test

--- src/sda/exercises/Problem1.java
+++ src/sda/exercises/Problem1.java
@@ -1,4 +1,26 @@
 package sda.exercises;
+/*
+Problem1: extract conditions from an expression
+Example: if(a>b&& b<c||d<e) extract a>b and b<c ...
+ */
+
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Problem1 {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String myString = myScanner.nextLine();
+
+        //System.out.println(Arrays.toString(new String[]{"||"}));
+
+        String[] myStringArray = myString.split("<|&&|>|'", 10);
+        for (String a: myStringArray)
+            System.out.println(a);
+
+
+    }
+
+
 }
