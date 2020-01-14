9a982cc7f580b1a000b2633ddb109d152c901151
Olimpia Balaci <olimpia.balaci@yahoo.com>
2020-01-14 18:15:31 +0200

    changes

--- src/sda/exercises/Operand.java
+++ src/sda/exercises/Operand.java
@@ -1,23 +1,63 @@
 package sda.exercises;
 
 public class Operand {
-  private Object left;
+    private Object left;
     private Object right;
     private String operator;
 
-    public Operand(String expresion) {
-        expresion = expresion.substring(1,expresion.length()-1);
-        String[] operator = {">","<","<=",">=","==","!="};
-        for (int i = 0; i < operator.length ; i++) {
-            String[] result = expresion.split(operator[i]);
-            if(result.length != 0)
-            {
+    public Operand(String expression) {
+        expression = expression.substring(1, expression.length() - 1);
+        String[] operator = {">", "<", "<=", ">=", "==", "!="};
+        for (int i = 0; i < operator.length; i++) {
+            String[] result = expression.split(operator[i]);
+            if (result.length != 0) {
                 this.left = result[0];
                 this.right = result[1];
                 this.operator = operator[i];
                 break;
             }
         }
+    }
+
+    public Object getLeft() {
+        return left;
+    }
+
+    public void setLeft(Object left) {
+        this.left = left;
+    }
+
+    public Object getRight() {
+        return right;
+    }
+
+    public void setRight(Object right) {
+        this.right = right;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public void setOperator(String operator) {
+        this.operator = operator;
+    }
+
+    public static String pickUpExpressions(String expressions) {
+        //if we have more than one expression, return all the expressions
+        //delete spaces
+        expressions = expressions.replace(" ", "");
+        //delete if word
+        expressions = expressions.replace("if", "");
+        if (expressions.length() > 4) {
+            return expressions;
+        } else {
+            expressions = expressions.substring(1, expressions.length() - 1);
+            //(a<b)&&(a>c)
+            //find && and ||
+            return expressions;
+        }
+
 
     }
 }
--- src/sda/exercises/Problem1.java
+++ src/sda/exercises/Problem1.java
@@ -9,13 +9,22 @@ import java.util.Scanner;
 
 public class Problem1 {
     public static void main(String[] args) {
+        //new
+        Operand myOperand = new Operand("(a>b)");
+
+        System.out.println(myOperand.getOperator());
+
+        System.out.println(myOperand.getLeft());
+        System.out.println(myOperand.getRight());
+
+
         Scanner myScanner = new Scanner(System.in);
         System.out.println("Enter a string: ");
         String myString = myScanner.nextLine();
 
         myString = myString.replace(" ", "");
         myString = myString.replace("if", "");
-        myString = myString.substring(1,myString.length()-1);
+        myString = myString.substring(1, myString.length() - 1);
         System.out.println(myString);
 
         //System.out.println(Arrays.toString(new String[]{"&&","||"}));
